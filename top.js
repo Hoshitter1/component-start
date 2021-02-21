4a9d6d9c1341742b1df7ab170f96b06a9c0f8b76
Hoshitter1 <hoshi.to.318@gmail.com>
2021-02-21 15:56:25 +0900

    added shared component

--- top.js
+++ top.js
@@ -2,3 +2,8 @@ const btnClicked = () => {
   window.location.href = "page1.html";
 };
 document.querySelector(".btn").addEventListener("click", btnClicked);
+
+const alertClicked = () => {
+  alert("alertがclickされたよ");
+};
+document.querySelector("#alert").addEventListener("click", alertClicked);
